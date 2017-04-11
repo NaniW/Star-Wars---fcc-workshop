5c459f96d56a7914afa681a1423dab185e22882c
ed42311 <ed42311@gmail.com>
2017-04-10 19:23:00 -0600

    this stuff

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-console.log("I MADE IT!!")
+console.log("I MADE IT!!");
+
+var app = new Vue({
+  el: '#component',
+  data: {
+    message: "Hello from vue!",
+    showMessage: true,
+    todos: [ {text: "learn VUE"},
+             {text: "thing"},
+             {text: "other thing"},
+           ]
+  },
+  methods: {
+    toggleMsg: function() {
+        this.showMessage = !this.showMessage;
+    }
+  }
+});
